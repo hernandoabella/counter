c4c50f209b6da2f5184f0c2a53972c436d9d465d
Hernando Abella <hernandoabella@gmail.com>
2024-04-25 00:25:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,53 @@
 // Variables
-let counter = 0; // Initialize counter variable to 0
-const number = document.getElementById('number'); // Get element with ID 'number'
-const button1 = document.getElementById('button1'); // Get element with ID 'button1'
-const button2 = document.getElementById('button2'); // Get element with ID 'button2'
+let counter = 0;
+const number = document.getElementById('number');
+const subtractButton = document.getElementById('subtractButton');
+const resetButton = document.getElementById('resetButton');
+const addButton = document.getElementById('addButton');
 
 const add = () => {
-   removeAnimationClass(); // Remove 'animation' and 'animation2' classes
+   removeAnimationClass();
    setTimeout(() => {
-      number.classList.add('animation'); // Add 'animation' class after 10 milliseconds
+      number.classList.add('animation');
    }, 10);
-   counter++; // Increment counter variable
-   testColor(); // Update number color
-   console.log(counter); // Print current counter value to console
-   number.innerText = counter; // Update 'number' element content with counter variable value
-   return counter; // Return current counter value
+   counter++;
+   testColor();
+   console.log(counter);
+   number.innerText = counter;
+   return counter;
 }
 
 const subtract = () => {
-   removeAnimationClass(); // Remove 'animation' and 'animation2' classes
+   removeAnimationClass();
    setTimeout(() => {
-      number.classList.add('animation2'); // Add 'animation2' class after 10 milliseconds
+      number.classList.add('animation2');
    }, 10);
-   counter--; // Decrement counter variable
-   testColor(); // Update number color
-   console.log(counter); // Print current counter value to console
-   number.innerText = counter; // Update 'number' element content with counter variable value
-   return counter; // Return current counter value
+   counter--;
+   testColor();
+   console.log(counter);
+   number.innerText = counter;
+   return counter;
 }
 
-// Remove 'animation' and 'animation2' classes
 const removeAnimationClass = () => {
    number.classList.remove('animation2', 'animation');
 }
 
 const testColor = () => {
    if (counter < 0) {
-      number.style.color = "var(--color2)"; // Use 'color2' for negative numbers
+      number.style.color = "var(--color2)";
    } else if (counter > 0) {
-      number.style.color = "var(--color1)"; // Use 'color1' for positive numbers
+      number.style.color = "var(--color1)";
    } else {
-      number.style.color = '#585757'; // Use '#585757' for number 0
+      number.style.color = '#585757';
    }
 }
 
-// Reset counter to 0 and update number color
 const reset = () => {
-   counter = 0; // Reset counter variable to 0
-   removeAnimationClass(); // Remove animation classes
-   testColor(); // Update number color
-   console.log(counter); // Print reset counter value to console
-   number.innerText = counter; // Update 'number' element content with counter variable value
-   return counter; // Return reset counter value
+   counter = 0;
+   removeAnimationClass();
+   testColor();
+   console.log(counter);
+   number.innerText = counter;
+   return counter;
 }
